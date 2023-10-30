153c477198027e30b60112faf54ba8c38b038735
reomin <takeuti29reo@gmail.com>
2023-10-30 19:57:53 +0900

    ログイン時にエラーのバリデーションと、メッセージの追加

---
+++ frontend/src/app/pages/home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+
+  return (
+    <div className="bg-yellow-400 dark:bg-gray-800 h-screen overflow-hidden flex items-center justify-center">
+       <h1>成功しました</h1>
+    </div>
+  )
+}
---
+++ frontend/src/app/pages/login.tsx
@@ -0,0 +1,75 @@
+"use client"
+import { useState, useEffect } from "react"
+import axios from "axios";
+import { useRouter } from "next/router";
+
+export default function Login() {
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [postedData, setPostedData] = useState("");
+    const router = useRouter();
+
+    const http = axios.create({
+        withCredentials: true,
+    });
+
+    console.log(http);
+    const onSubmitHandler = async (e:any) => {
+        e.preventDefault()
+
+        //apiのuserController login関数に接続
+        // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+        //   method: 'POST',
+        //   headers: {
+        //     'Content-Type': 'application/json'
+        //   },
+        //   body: JSON.stringify({ name}),
+        // })
+    
+        // const data = await res.json()
+        // setPostedData(data.body)
+        axios.get('http://localhost:8000/sanctum/csrf-cookie', { withCredentials: true })
+        .then((res) => {
+          // ログイン処理
+          http.post('http://localhost:8000/api/login', { email, password })
+            .then((res) => {
+              router.push("/home");
+            })
+            .catch((err) => {
+              alert("エラーが発生しました");
+              console.error(err.message);
+            });
+        })
+        .catch((err) => {
+          alert("エラーが発生しました");  
+          console.error(err.message);
+        });
+        }
+
+
+  return (
+    <div className="bg-yellow-400 dark:bg-gray-800 h-screen overflow-hidden flex items-center justify-center">
+        <div className="bg-white lg:w-6/12 md:7/12 w-8/12 shadow-3xl rounded-xl">
+            <div className="bg-gray-800 shadow shadow-gray-200 absolute left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full p-4 md:p-8">
+                <h1>Lending Management App</h1>
+            </div>
+            <form onSubmit={onSubmitHandler} className="p-12 md:p-24">
+            <div className="flex items-center text-lg mb-6 md:mb-8">
+                <svg className="absolute ml-3" width="24" viewBox="0 0 24 24">
+                <path d="M20.822 18.096c-3.439-.794-6.64-1.49-5.09-4.418 4.72-8.912 1.251-13.678-3.732-13.678-5.082 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-3.073.71-3.188 2.236-3.178 4.904l.004 1h23.99l.004-.969c.012-2.688-.092-4.222-3.176-4.935z"/>
+                </svg>
+                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" id="username" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
+            </div>
+            <div className="flex items-center text-lg mb-6 md:mb-8">
+                <svg className="absolute ml-3" viewBox="0 0 24 24" width="24">
+                <path d="m18.75 9h-.75v-3c0-3.309-2.691-6-6-6s-6 2.691-6 6v3h-.75c-1.24 0-2.25 1.009-2.25 2.25v10.5c0 1.241 1.01 2.25 2.25 2.25h13.5c1.24 0 2.25-1.009 2.25-2.25v-10.5c0-1.241-1.01-2.25-2.25-2.25zm-10.75-3c0-2.206 1.794-4 4-4s4 1.794 4 4v3h-8zm5 10.722v2.278c0 .552-.447 1-1 1s-1-.448-1-1v-2.278c-.595-.347-1-.985-1-1.722 0-1.103.897-2 2-2s2 .897 2 2c0 .737-.405 1.375-1 1.722z"/>
+                </svg>
+                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" id="password" className="bg-gray-200 rounded pl-12 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
+            </div>
+            <button type = "submit" className="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full rounded">Login</button>
+            </form>
+        </div>
+    </div>
+  )
+}
