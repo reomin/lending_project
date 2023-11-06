0d3482b8688e27566189a58e95045b4d5d745f4d
reomin <takeuti29reo@gmail.com>
2023-11-06 21:53:05 +0900

    返却ボタンのレイアウトを作成

--- frontend/src/app/components/ItemList.tsx
+++ frontend/src/app/components/ItemList.tsx
@@ -14,6 +14,11 @@ import { Items } from "@/types/items";
     }
   ]
 
+  //idを渡す
+  const lendItem =(e: Number)=>{
+    console.log(e);
+  }
+
 function ItemLists(props: {items: Items[]}) {
     return (
       <div>
@@ -22,7 +27,10 @@ function ItemLists(props: {items: Items[]}) {
                 <div key={item.id}>
                     <li className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow mb-5">
                     <h1>{item.title}</h1>
-                    <h1 className="flex">{item.stock}</h1>
+                    <div className="flex">
+                        <h1>{item.stock}</h1>
+                        <button onClick={(e) => lendItem(item.id)} className="mx-3 bg-blue-600 text-white rounded-lg p-1">貸出</button>
+                    </div>
                     </li>
                 </div>
             )
